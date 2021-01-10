bdbbf68460cc99e6354b1eadb767d39ccc820b0f
lukenorys <lukenorys@ymail.com>
2021-01-09 18:15:07 -0600

    add enemy to fight sequence

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,7 +17,11 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
+      // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+    // place fight function code block here . . .
+
+    // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
@@ -69,11 +73,15 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+  }
+
 };
 
 // run fight function to start game
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
+}
 //fight();
